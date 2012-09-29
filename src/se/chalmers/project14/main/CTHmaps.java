48126db9e7c846859e91c45b57217cc17d9fc5c0
epoxy <palmqvist.anton@gmail.com>
2012-09-29 17:27:25 +0200

    Bug01

--- src/se/chalmers/project14/main/CTHmaps.java
+++ src/se/chalmers/project14/main/CTHmaps.java
@@ -3,9 +3,10 @@ package se.chalmers.project14.main;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 
-import se.chalmers.project14.main.R;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
 
 public class CTHmaps extends MapActivity {
@@ -17,6 +18,11 @@ public class CTHmaps extends MapActivity {
         //Enabling zooming
         MapView mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
+        
+        /*Using the LocationManager class to obtain GPS-location*/
+        LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locListener = new MyLocationListener(this);
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
     }
 
     @Override
---
+++ src/se/chalmers/project14/main/MyLocationListener.java
@@ -0,0 +1,34 @@
+package se.chalmers.project14.main;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MyLocationListener implements LocationListener {
+	private Context context;
+	
+	public MyLocationListener(Context context) {
+		this.context=context;
+	}
+	
+	public void onLocationChanged(Location location) {
+		String text = "Min nuvarande position är: \nLatitud: " + location.getLatitude() + 
+				"\nLongitud: " + location.getLongitude();		
+		Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+	}
+
+	public void onProviderDisabled(String provider) {
+		Toast.makeText(context, "GPS Disabled", Toast.LENGTH_SHORT).show();
+	}
+
+	public void onProviderEnabled(String provider) {
+		Toast.makeText(context, "GPS Enabled", Toast.LENGTH_SHORT).show();
+	}
+
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+	}
+
+}
