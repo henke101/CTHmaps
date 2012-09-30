8cc6d151d249d1646d482f5988c93676bef1eae9
Tomas <tomsel1@hotmail.com>
2012-09-30 16:17:04 +0200

    refactoring the code

--- src/se/chalmers/project14/main/CTHmaps.java
+++ src/se/chalmers/project14/main/CTHmaps.java
@@ -20,40 +20,6 @@ public class CTHmaps extends MapActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
-		DatabaseHandler db = new DatabaseHandler(this);
-		Coordinates coordinates = new Coordinates();
-		// Intent intent = getIntent();
-		// String cthLocation = intent
-		// .getStringExtra(ChooseLocationActivity.EXTRA_MESSAGE);
-
-		Log.d("inserting ", "is");
-		// Log.d(" input is ", cthLocation);
-		// db.addCordinates(new Coordinates("Edithuset", "57.68744,11.979491"));
-
-		Log.d("have", "added Edithuset");
-		coordinates = db.getCoordinates("Tomas");
-		Log.d("invoked", "getCoordinates Tomas");
-		String cthPlace = coordinates.getCTHplace();
-		String coord = coordinates.getCoordinates();
-		Log.d("From CTHmaps " + cthPlace, "From database getCoordinate method "
-				+ coord);
-		coordinates = db.getCoordinates("FirstTry");
-		Log.d("FirstTry getting the coordinates from database ", ""
-				+ coordinates.getID() + " " + coordinates.getCTHplace() + " "
-				+ coordinates.getCoordinates());
-		coordinates = db.getCoordinates("AdamOchEva");
-		Log.d("AdamOchEva getting the coordinates from database ", ""
-				+ coordinates.getID() + " " + coordinates.getCTHplace() + " "
-				+ coordinates.getCoordinates());
-		coordinates = db.getCoordinates("SecondTry");
-		Log.d("SecondTry getting the coordinates from database ", ""
-				+ coordinates.getID() + " " + coordinates.getCTHplace() + " "
-				+ coordinates.getCoordinates());
-		List<Coordinates> coordinateList = db.getAllCoordinates();
-		for (Coordinates c : coordinateList) {
-			String log = (" " + c.getCTHplace());
-			Log.d("Name: ", log);
-		}
 		// Enabling zooming*/
 		MapView mapView = (MapView) findViewById(R.id.mapview);
 		mapView.setBuiltInZoomControls(true);
