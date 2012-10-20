b94460a8397d92b18647e94ce8aa0aeac8d37648
Tomas <tomsel1@hotmail.com>
2012-10-20 22:43:04 +0200

    added test for class House simple test just containing getters and setters

---
+++ tests/src/se/chalmers/project14/test/TestHouse.java
@@ -0,0 +1,30 @@
+package se.chalmers.project14.test;
+
+import se.chalmers.project14.model.House;
+import junit.framework.TestCase;
+
+public class TestHouse extends TestCase {
+
+	private House house = new House();
+
+	public void TestGetId() {
+		house.setId(0);
+		int getId = house.getId();
+		int expectedResult = 0;
+		assertEquals(expectedResult, getId);
+	}
+
+	public void TestGetFloor() {
+		house.SetFloor("5");
+		String getFloor = house.getFloor();
+		String expectedResult = "5";
+		assertEquals(expectedResult, getFloor);
+	}
+
+	public void TestGetLectureRoom() {
+		house.setLectureRoom("hubben");
+		String getLectureRoom = house.getLectureRoom();
+		String expectedResult = "hubben";
+		assertEquals(expectedResult, getLectureRoom);
+	}
+}
