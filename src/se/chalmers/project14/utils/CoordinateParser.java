ecce8b3a4f6e6d0b4d881e2af5f8d4daeeab0b9f
Henrik Andersson <heenrik@hotmail.com>
2012-10-22 20:49:50 +0200

    Added javadoc to CoordinateParser, BuildingOverlay and OverlayHolder

--- src/se/chalmers/project14/utils/CoordinateParser.java
+++ src/se/chalmers/project14/utils/CoordinateParser.java
@@ -11,11 +11,18 @@ public class CoordinateParser {
 	private CoordinateParser() {
 
 	}
-
+	/**
+	 * Returns the singleton instance of CoordinateParser
+	 * @return CoordinateParser
+	 */
 	public static CoordinateParser getInstance() {
 		return parser;
 	}
-
+	/**
+	 * Parses from a string of coordinates separated by a "," into an array of coordinates
+	 * @param s The string of coordinates
+	 * @return int []
+	 */
 	public int[] parseCoordinatesFromString(String s) {
 		String[] coordinatesString = s.split(",");
 		int[] coordinatesInt = new int[coordinatesString.length];
